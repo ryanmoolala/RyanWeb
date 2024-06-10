e5d953c8b0a6ac85adfa215e0ae7b3be14ecbfb8
ryanmoolala <ryantjy1001@gmail.com>
2024-06-10 18:41:47 +0800

    react + tailwind

---
+++ website/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import Top from './Components/Top';
+
+function App() {
+  return (
+    
+    <div>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+      <Top/>
+      
+    </div>
+  );
+}
+
+/*upon entering the url
+
+Dashboard page will have
+1. Top hub that stays fixed
+a. My Name
+b. Home section .home()
+c. About me section .about()
+d. My skills + projects .skills()
+e. Work experience .work()
+*/
+
+export default App;
---
+++ website/src/Components/Top.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Top = () => {
+    return (
+        <div>
+
+            <div class="bg-black">Logo</div>
+
+            <div class="flex-col">
+                
+                <button>.home()</button>
+                <button>.about()</button>
+                <button>.skills()</button>
+                <button>.work()</button>
+ 
+            </div>
+        </div>
+    )
+}
+
+export default Top;
