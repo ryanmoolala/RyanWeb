70b6071ff048fe59fec944f02808cf4be999bc56
ryantjy <ryantjy2002@gmail.com>
2024-06-14 22:08:56 +0800

    .

--- src/Components/Top.jsx
+++ src/Components/Top.jsx
@@ -19,13 +19,14 @@ const Top = ({setBodyNumber}) => {
 
       <div class="flex justify-between w-1/2 border-b-2 mt-3 border-red-500 rounded-sm space-x-5 min-w-fit">
 
-      <div class="bg-white rounded-full ml-8 w-20 h-20  mb-1 flex-shrink-0 text-7xl font-bold font-roboto-slab text-center pt-1">R</div>
+      <div class="bg-white rounded-full w-20 h-20  mb-1 flex-shrink-0 text-7xl font-bold font-roboto-slab text-center pt-1">R</div>
 
-      <div class="flex flex-row space-x-14 mr-20 text-lg font-roboto-mono">
+      <div class="flex flex-row space-x-1 mr-20 text-lg font-roboto-mono md:space-x-14">
         {isActive == 0 ? <button class="text-red-500 mb-4 mt-5" id="button" onClick={() => pressButton(0)}>.home()</button> : <button class="text-zinc-200 mb-4 mt-5" onClick={() => pressButton(0)}>.home()</button>}
         {isActive == 1 ? <button class="text-red-500 mb-4 mt-5" id="button" onClick={() => pressButton(1)}>.about()</button> : <button class="text-zinc-200 mb-4 mt-5" onClick={() => pressButton(1)}>.about()</button>}
         {isActive == 2 ? <button class="text-red-500 mb-4 mt-5" id="button" onClick={() => pressButton(2)}>.skills()</button> : <button  class="text-zinc-200 mb-4 mt-5" onClick={() => pressButton(2)}>.skills()</button>}
       </div>
+
       </div>
 
     </div>
