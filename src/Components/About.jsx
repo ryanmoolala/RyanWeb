b5ffd615ce62998df399e4700c4773eb2f07257d
ryantjy <ryantjy2002@gmail.com>
2024-07-01 10:58:49 +0800

    .

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -46,8 +46,11 @@ const About = () => {
                         </span>
                     <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-300">NUS SoC<span class="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ms-3">Latest</span></h3>
                   <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Currently studying Computer Science until 2027</time>
-                  <p class="text-base font-normal">Awarded NUS Merit Scholarship</p>        
-            
+                  <p class="text-base font-normal">Awarded NUS Merit Scholarship</p>     
+                  <br></br>
+                  <p class="text-base font-normal">NUS Orbital, a SWE summer project</p>       
+        
+
                     </li>
                 
                    <li class="mb-14 ms-6">
@@ -62,10 +65,8 @@ const About = () => {
         <br/>
         <p class="text-base font-normal ">Attained 6 Distinctions & 2 Scholarships for good academic & leadership performance</p>
         <br/>
-        <p class="text-base font-normal ">Got into National University of Singapore (NUS), Computer Science</p>
+        <p class="text-base font-normal ">Enrolled into National University of Singapore (NUS), Computer Science</p>
                    </li>
-
-
                    <li class="ms-6">
                     <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
                <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
