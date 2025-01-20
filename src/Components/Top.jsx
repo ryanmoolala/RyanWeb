710609e96e4f9df5ee1a8df2bdf298bc78779363
ryanmoolala <ryantjy1001@gmail.com>
2025-01-20 22:24:26 +0800

    .

--- src/Components/Top.jsx
+++ src/Components/Top.jsx
@@ -11,10 +11,7 @@ const Top = ({setBodyNumber}) => {
 
   return (
     <div class="flex justify-center">
-      <div class="flex justify-between w-1/2 border-b-2 mt-3 border-red-500 rounded-sm space-x-5 min-w-fit">
-        <div class="bg-white rounded-full w-20 h-20  mb-1 flex-shrink-0 text-7xl font-bold font-roboto-slab text-center pt-1">R</div>
-
-        <nav class="flex flex-row space-x-1 mr-20 text-lg font-roboto-mono md:space-x-14">
+      <div class="flex w-1/3 border-b-2 mt-3 border-red-500 rounded-sm space-x-5 min-w-fit justify-between text-sm font-roboto-mono md:text-lg">
           {isActive == 0 ? 
             <button class="text-red-500 md:text-red-500 mb-4 mt-5" id="button" onClick={() => pressButton(0)}>.home()</button> : 
             <button class="text-white md:text-white mb-4 mt-5" onClick={() => pressButton(0)}>.home()</button>}
@@ -30,7 +27,6 @@ const Top = ({setBodyNumber}) => {
           {isActive == 3 ? 
             <button class="text-red-500 md:text-red-500 mb-4 mt-5" id="button" onClick={() => pressButton(3)}>.experience()</button> : 
             <button class="text-white md:text-white mb-4 mt-5" onClick={() => pressButton(3)}>.experience()</button>}
-        </nav>
       </div>
     </div>
   );
